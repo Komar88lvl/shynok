9c40de2bb186566bcb9d90dc657aaf56fd54423d
Sviatoslav <djsv91@gmail.com>
2025-04-07 16:57:47 +0300

    Set Cook model`s custom fields in admin panel
    - Add Qualification fieldset to Cook info
    - Add Additional info fieldset to Cook create
    - Use admin.register decorator in admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -4,12 +4,24 @@ from django.contrib.auth.admin import UserAdmin
 from kitchen.models import DishType, Dish, Cook
 
 
+@admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     list_display = ["name", "dish_type", "price",]
     list_filter = ["dish_type__name", ]
     search_fields = ["name", ]
 
 
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Qualification", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {
+            "fields": ("first_name", "last_name", "years_of_experience",)
+        }),
+    )
+
+
 admin.site.register(DishType)
-admin.site.register(Dish, DishAdmin)
-admin.site.register(Cook, UserAdmin)
