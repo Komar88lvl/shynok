3f8631dc8c97c977e51cc9501dbbe0c51602d133
Sviatoslav <djsv91@gmail.com>
2025-04-08 16:26:52 +0300

    Add index view with number of models

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, DishType, Dish
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_cooks = Cook.objects.count()
+    num_type_dishes = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    context = {
+        "num_cooks": num_cooks,
+        "num_type_dishes": num_type_dishes,
+        "num_dishes": num_dishes,
+    }
+    return render(request, "kitchen/index.html", context=context)
