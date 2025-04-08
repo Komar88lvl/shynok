e1b3c81a1b5a0cf54823ded32690a0b7f3d21eb9
Sviatoslav <djsv91@gmail.com>
2025-04-08 20:12:40 +0300

    Create Dish Type view and url dish-types/
    - View is class based
    - Has custom templates name and context name

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Cook, DishType, Dish
 
@@ -14,3 +15,9 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_dishes": num_dishes,
     }
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_type_list"
