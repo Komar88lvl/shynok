597235435a6dbf49738518175718f6e72a54673c
Sviatoslav <djsv91@gmail.com>
2025-04-09 21:32:04 +0300

    Add links to dish_type_list template

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from kitchen.views import(
+from kitchen.views import (
        index,
        DishTypeListView,
        DishListView,
@@ -10,9 +10,16 @@ from kitchen.views import(
 
 urlpatterns = [
        path("", index, name="index"),
+
        path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
-       path("dish-types/<int:pk>/", DishTypeDetailView.as_view(), name="dish-type-detail"),
+       path(
+              "dish-types/<int:pk>/",
+              DishTypeDetailView.as_view(),
+              name="dish-type-detail"
+       ),
+
        path("dishes/", DishListView.as_view(), name="dish-list"),
+
        path("cooks/", CookListView.as_view(), name="cook-list"),
 ]
 
