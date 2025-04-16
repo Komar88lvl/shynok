a05e83b6c31bcc4c795e42516564de7f1773d39f
Sviatoslav <djsv91@gmail.com>
2025-04-16 16:38:33 +0300

    Overrode get_context_data and get_queryset for DishListview

--- kitchen/views.py
+++ kitchen/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from kitchen.forms import CookCreationForm, DishForm
+from kitchen.forms import CookCreationForm, DishForm, DishSearchForm
 from kitchen.models import Cook, DishType, Dish
 
 
@@ -74,6 +74,24 @@ class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     queryset = Dish.objects.select_related("dish_type")
     paginate_by = 5
+
+    def get_context_data(
+        self, *, object_list=..., **kwargs
+    ):
+        context = super(DishListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = DishSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        form = DishSearchForm(self.request.GET)
+        if form.is_valid():
+            return self.queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return self.queryset
 
 
 class DishDetailView(LoginRequiredMixin, generic.DetailView):
