b718971c3cea753c1b650a331e731207d5df98f2
Sviatoslav <djsv91@gmail.com>
2025-04-17 18:28:58 +0300

    Create str representation test for Cook model

--- kitchen/tests.py
+++ kitchen/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from kitchen.models import DishType, Dish
@@ -8,5 +9,14 @@ class ModelTests(TestCase):
         dish_type = DishType.objects.create(name="test")
         self.assertEqual(str(dish_type), dish_type.name)
 
-    # def dish_str(self):
-    #     dish = Dish.objects.create()
+    def test_cook_str(self):
+        cook = get_user_model().objects.create(
+            username="test",
+            password="test123",
+            first_name="test_first",
+            last_name="test_last",
+        )
+        self.assertEqual(
+            str(cook),
+            f"{cook.username} ({cook.first_name} {cook.last_name})"
+        )
