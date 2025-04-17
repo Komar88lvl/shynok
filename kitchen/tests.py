9bcf2b6091e653f857c3ae2462cfdbf457cd571b
Sviatoslav <djsv91@gmail.com>
2025-04-17 18:19:36 +0300

    Create model test for DishType

--- kitchen/tests.py
+++ kitchen/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from kitchen.models import DishType, Dish
+
+
+class ModelTests(TestCase):
+    def test_dish_type_str(self):
+        dish_type = DishType.objects.create(name="test")
+        self.assertEqual(str(dish_type), dish_type.name)
+
+    # def dish_str(self):
+    #     dish = Dish.objects.create()
