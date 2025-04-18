8f29a4f3c690ffd767acc29fe9c9bffbc6e83afc
Sviatoslav <djsv91@gmail.com>
2025-04-18 14:45:19 +0300

    Create Admin tests and configure setUp case for tests

---
+++ kitchen/tests/test_admin.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="admin",
+            password="testadmin",
+        )
+        self.client.force_login(self.admin_user)
+        self.cook = get_user_model().objects.create_user(
+            username="cook",
+            password="testcook",
+            years_of_experience = 3,
+        )
