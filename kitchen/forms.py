e96dccb3910a21f1b2ea29f9454834d7fc4abeaa
Sviatoslav <djsv91@gmail.com>
2025-04-23 15:31:09 +0300

    Create search form for DishType name

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -51,3 +51,16 @@ class CookSearchForm(forms.Form):
             }
         )
     )
+
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by dish type name"
+            }
+        )
+    )
