3ea7918cf99b5dde13a04dd8545f3e445fddf486
Sviatoslav <djsv91@gmail.com>
2025-04-23 15:15:53 +0300

    Create search form for Cook username

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -38,3 +38,16 @@ class DishSearchForm(forms.Form):
             }
         )
     )
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by cook username"
+            }
+        )
+    )
