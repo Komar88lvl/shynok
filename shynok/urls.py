66eedefe01a58acdb811b839591825de4119a91d
Sviatoslav <djsv91@gmail.com>
2025-04-27 15:28:29 +0300

    Remove django debug toolbar from project

--- shynok/urls.py
+++ shynok/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
        path('admin/', admin.site.urls),
        path("kitchen/", include("kitchen.urls", namespace="kitchen")),
        path("accounts/", include("django.contrib.auth.urls"))
-] + debug_toolbar_urls()
+]
