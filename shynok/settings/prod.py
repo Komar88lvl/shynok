df67819c79c593400c954b899bc92db79a00d1bb
Sviatoslav <djsv91@gmail.com>
2025-05-01 19:48:37 +0300

    Connect postgresql DB & separate settings to dev and prod

---
+++ shynok/settings/prod.py
@@ -0,0 +1,22 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": int(os.environ["PGPORT"]),
+    }
+}
